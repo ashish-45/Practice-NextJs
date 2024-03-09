2bf99fe40d5389fe2fbbf93e6653a6c5825e890f
ashish-45 <ashishnirvikar5670@gmail.com>
2024-03-09 15:59:33 +0530

    next js basic started

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <>
+            <h1 className="text-center font-bold text-green-500 mt-5">Hello Ashish</h1>
+        </>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+        <p className="text-blue-400 pl-2">Parent Layout</p>
+        {children}
+
+      </body>
     </html>
   );
 }
